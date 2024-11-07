39faa7598393ceb6e437635d7bf66125318b29bc
Xhram <87285044+Xhram@users.noreply.github.com>
2024-11-06 20:01:08 -0600

    Update version to 1.0.8 and fix typo in error message

--- src/globals.c
+++ src/globals.c
@@ -10,7 +10,7 @@ const char piChar = (char)(128);
 const char sqrtChar = (char)(130);
 const char upArrowChar = (char)(24);
 const char squareChar = (char)(131);
-const char version[10] = "V:1.0.7";
+const char version[10] = "V:1.0.8";
 
 
 const uint8_t selectColor = 239;
